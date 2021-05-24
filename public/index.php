25c915933ffc0fd529c4203e1cce4269bf9aa1a7
Evg <budo@narod.ru>
2021-05-24 09:03:20 +0300

    FIX: Content Security Policy

--- public/index.php
+++ public/index.php
@@ -13,7 +13,7 @@ define('HLEB_PUBLIC_DIR', __DIR__);
 // Общие заголовки.
 // Content Security Policy
 $_SERVER['nonce'] = bin2hex(random_bytes('12'));
-header("Content-Security-Policy: default-src 'self' https://www.google.com  https://www.gstatic.com; img-src 'self' blob:; object-src 'none'; style-src 'self' 'nonce-".$_SERVER['nonce']."'");
+header("Content-Security-Policy: default-src 'self' https://www.google.com  https://www.gstatic.com; script-src 'self' 'nonce-".$_SERVER['nonce']."'; img-src 'self' blob:; object-src 'none'; style-src 'self' 'nonce-".$_SERVER['nonce']."'");
 header("Strict-Transport-Security: max-age=31536000; includeSubDomains; preload;");
 header("Referrer-Policy: no-referrer-when-downgrade");
 header("X-XSS-Protection: 1; mode=block");
