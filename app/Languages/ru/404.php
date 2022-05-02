e3f036d61e88ddaaba6fe9d33d852152570c7013
Evg <budo@narod.ru>
2022-05-02 15:04:15 +0300

    DEV: Локализация (2)

--- app/Languages/ru/404.php
+++ app/Languages/ru/404.php
@@ -1,11 +1,9 @@
 <?php
 
 /*
-|
-| The following language strings are used on the 404 Error page.
-|
-| Следующие языковые строки используются на странице Ошибка 404.
-|
+* The following language strings are used on the 404 Error page.
+*
+* Следующие языковые строки используются на странице Ошибка 404.
 */
 
 return [
