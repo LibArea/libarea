582a7426a01a0c38f82355b2070c68d18e38c13e
Evg <budo@narod.ru>
2022-06-01 23:03:11 +0300

    DEV: Упростим admin.js

--- public/assets/js/admin.js
+++ public/assets/js/admin.js
@@ -1,85 +1,49 @@
 // I have read the complaint
 document.querySelectorAll(".report-saw")
   .forEach(el => el.addEventListener("click", function (e) {
-    let report_id = el.dataset.id;
-    fetch("/mod/admin/reports/saw", {
-      method: "POST",
-      body: "id=" + report_id,
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-    })
-      .then((response) => {
-        location.reload();
-      })
+    fetcherPost("/mod/admin/reports/saw", options = { body: "id=" + el.dataset.id })
   }));
 
 // Write down a Favicon
 document.querySelectorAll(".add-favicon")
   .forEach(el => el.addEventListener("click", function (e) {
-    let link_id = el.dataset.id;
-    fetch("/mod/admin/favicon/add", {
-      method: "POST",
-      body: "id=" + link_id,
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-    })
-      .then((response) => {
-        location.reload();
-      })
+    fetcherPost("/mod/admin/favicon/add", options = { body: "id=" + el.dataset.id })
   }));
 
 // Ban / unban: user / word 
 document.querySelectorAll(".type-ban")
   .forEach(el => el.addEventListener("click", function (e) {
-    let type_id = el.dataset.id;
-    let type = el.dataset.type;
-    fetch("/mod/admin/" + type + "/ban", {
-      method: "POST",
-      body: "id=" + type_id,
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-    })
-      .then((response) => {
-        location.reload();
-      })
+    fetcherPost("/mod/admin/" + el.dataset.type + "/ban", options = { body: "id=" + el.dataset.id })
   }));
 
 // Remove reward
 document.querySelectorAll(".remove-badge")
   .forEach(el => el.addEventListener("click", function (e) {
-    let id = el.dataset.id;
-    let uid = el.dataset.uid;
-    fetch("/mod/admin/badge/remove", {
-      method: "POST",
-      body: "id=" + id + "&uid=" + uid,
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-    })
-      .then((response) => {
-        location.reload();
-      })
+    fetcherPost("/mod/admin/badge/remove", options = { body: "id=" + el.dataset.id + "&uid=" + el.dataset.uid })
   }));
 
 // Content Recovery
 document.querySelectorAll(".audit-status")
   .forEach(el => el.addEventListener("click", function (e) {
-    let status_id = el.dataset.id;
-    let status_type = el.dataset.status;
-    fetch("/mod/admin/audit/status", {
-      method: "POST",
-      body: "status=" + status_id + "@" + status_type,
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-    })
-      .then((response) => {
-        location.reload();
-      })
+    fetcherPost("/mod/admin/audit/status", options = { body: "status=" + el.dataset.id + "@" + el.dataset.status })
   }));
-  
+
+// Data update  
 document.querySelectorAll(".update")
   .forEach(el => el.addEventListener("click", function (e) {
-    let type = el.dataset.type;
-    fetch("/mod/admin/manual/update", {
-      method: "POST",
-      body: "type=" + type,
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+    fetcherPost("/mod/admin/manual/update", options = { body: "type=" + el.dataset.type })
+  }));
+
+function fetcherPost(url, options = {}) {
+  return fetch(url, {
+    ...options,
+    method: "POST",
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+  })
+    .then((response) => {
+      location.reload();
     })
-      .then((response) => {
-          location.reload();
-      });
-  }));  
+}
+
+
+
