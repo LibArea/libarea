f2adfee412ed63f861c67db1412a99d86ba89eae
Evg <budo@narod.ru>
2022-06-03 00:35:24 +0300

    FIX: Незначительные изменения

--- modules/search/view/default/footer.php
+++ modules/search/view/default/footer.php
@@ -3,8 +3,6 @@
   <?= config('meta.name'); ?> &copy; <?= date('Y'); ?> — <span class="lowercase"><?= __('search.name'); ?></span>
 </footer>
 
-<a class="up_down_btn none" title="<?= __('search.up'); ?>">&uarr;</a>
-
 <script src="/assets/js/common.js"></script>
 
 </body>
