8e236e692c9e855b9ed02e45187a7d872715ae01
Evg <budo@narod.ru>
2022-06-16 22:54:10 +0300

    FIX: zoom в каталоге

--- resources/views/default/global/item.footer.php
+++ resources/views/default/global/item.footer.php
@@ -2,7 +2,7 @@
   <?= config('meta.name'); ?> &copy; <?= date('Y'); ?> — <span class="lowercase"><?= __('web.main_title'); ?></span>
 </footer>
 
-<script src="/assets/js/zoom/medium-zoom.min.js"></script>
+<script src="/assets/js/medium-zoom.js"></script>
 
 <script nonce="<?= $_SERVER['nonce']; ?>">
   document.addEventListener('DOMContentLoaded', () => {
