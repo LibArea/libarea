8f49ec2aa183af4ee98ded95091437997c41c8b0
Evg <budo@narod.ru>
2022-07-15 17:19:37 +0300

    UX: Локализация

--- resources/views/default/_block/form/multilingual-field.php
+++ resources/views/default/_block/form/multilingual-field.php
@@ -1,13 +1,13 @@
 <fieldset class="tabs" id="<?= $name; ?>">
   <div class="tabs__nav">
     <?php foreach (config('general.languages') as $key => $lang) : ?>
-      <button class="tabs__btn<?php if (config('general.lang') == $lang) { ?> tabs__btn_active<?php } ?>"><?= __('app.' . $lang . '_language'); ?></button>
+      <button class="tabs__btn<?php if (config('general.lang') == $lang) { ?> tabs__btn_active<?php } ?>"><?= $lang; ?></button>
     <?php endforeach; ?>
   </div>
   <div class="tabs__content">
     <?php foreach (config('general.languages') as $key => $lang) : ?>
       <div class="tabs__pane<?php if (config('general.lang') == $lang) { ?> tabs__pane_show<?php } ?>">
-        <label for="<?= $name; ?>"><?= __('app.' . $name); ?> (<?= $lang; ?>)<sup class="red">*</sup></label>
+        <label for="<?= $name; ?>"><?= __('app.' . $name); ?> <span class="help">(<?= __('app.' . $lang . '_language'); ?>)</span> <sup class="red">*</sup></label>
         <input minlength="3" type="text" name="lang[<?= $lang; ?>][<?= 'translation_' . $name; ?>]" value="<?= $arr[$key]['translation_' . $name] ?? ''; ?>">
         <div class="help">> 3 <?= __('app.characters'); ?></div>
       </div>
