dc79f15f46608069fb70687f366081f85d50ddc5
Evg <budo@narod.ru>
2022-10-24 15:45:16 +0300

    UX: Мобильная версия (комментарии)

--- resources/views/default/content/comment/comment.php
+++ resources/views/default/content/comment/comment.php
@@ -5,31 +5,33 @@ foreach ($comments as $comment) :
 
   <?php if (!empty($comment['answer_id'])) : ?>
 
-    <div class="flex gap-min items-center text-sm mb5 gray-600">
-      <a class="gray-600 flex items-center" href="<?= url('profile', ['login' => $comment['login']]); ?>">
-        <?= Img::avatar($comment['avatar'], $comment['login'], 'img-sm mr5', 'small'); ?>
-        <?= $comment['login']; ?>
-      </a>
-      <span class="lowercase"><?= Html::langDate($comment['answer_date']); ?></span> |
-      <span class="lowercase"><?= __('app.in'); ?>:</span>
-      <a class="gray-600" href="<?= url('post', ['id' => $comment['post_id'], 'slug' => $comment['post_slug']]); ?>#answer_<?= $comment['answer_id']; ?>">
+    <div class="flex gap-min text-sm mb5 gray-600">
+      <div class="flex gap-min">
+        <a class="gray flex gap-min" href="<?= url('profile', ['login' => $comment['login']]); ?>">
+          <?= Img::avatar($comment['avatar'], $comment['login'], 'img-sm', 'small'); ?>
+          <?= $comment['login']; ?>
+        </a>
+        <span class="lowercase"><?= Html::langDate($comment['answer_date']); ?></span> |
+      </div>
+      <a class="gray-600 cut-off" href="<?= url('post', ['id' => $comment['post_id'], 'slug' => $comment['post_slug']]); ?>#answer_<?= $comment['answer_id']; ?>">
         <?= $comment['post_title']; ?>
       </a>
     </div>
-       
+
     <div class="right"><?= Html::votes($comment, 'answer'); ?></div>
-    <?= markdown($comment['answer_content']); ?>
+    <div class="cut-post"><?= markdown($comment['answer_content']); ?></div>
 
   <?php else : ?>
 
-    <div class="flex gap-min items-center text-sm mb5 gray-600">
-      <a class="gray-600 flex items-center" href="<?= url('profile', ['login' => $comment['login']]); ?>">
-        <?= Img::avatar($comment['avatar'], $comment['login'], 'img-sm mr5', 'small'); ?>
-        <?= $comment['login']; ?>
-      </a>
-      <span class="lowercase"><?= Html::langDate($comment['comment_date']); ?></span> |
-      <span class="lowercase"><?= __('app.in'); ?>:</span>
-      <a class="gray-600" href="<?= url('post', ['id' => $comment['post_id'], 'slug' => $comment['post_slug']]); ?>#comment_<?= $comment['comment_id']; ?>">
+    <div class="flex gap-min text-sm mb5 gray-600">
+      <div class="flex gap-min">
+        <a class="gray flex gap-min" href="<?= url('profile', ['login' => $comment['login']]); ?>">
+          <?= Img::avatar($comment['avatar'], $comment['login'], 'img-sm', 'small'); ?>
+          <?= $comment['login']; ?>
+        </a>
+        <span class="lowercase"><?= Html::langDate($comment['comment_date']); ?></span> |
+      </div>
+      <a class="gray-600 cut-off" href="<?= url('post', ['id' => $comment['post_id'], 'slug' => $comment['post_slug']]); ?>#comment_<?= $comment['comment_id']; ?>">
         <?= $comment['post_title']; ?>
       </a>
     </div>
