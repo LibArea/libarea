a77bdaf506c17214a635b64406c2001cf3b03944
Evg <budo@narod.ru>
2022-10-29 07:55:34 +0300

    UX: Страницы "Все комментарии"

--- resources/views/default/content/comment/comment.php
+++ resources/views/default/content/comment/comment.php
@@ -5,40 +5,41 @@ foreach ($comments as $comment) :
 
   <?php if (!empty($comment['answer_id'])) : ?>
 
-    <div class="flex gap-min text-sm mb5 gray-600">
-      <div class="flex gap-min">
-        <a class="gray flex gap-min" href="<?= url('profile', ['login' => $comment['login']]); ?>">
-          <?= Img::avatar($comment['avatar'], $comment['login'], 'img-sm', 'small'); ?>
-          <?= $comment['login']; ?>
-        </a>
-        <span class="lowercase"><?= Html::langDate($comment['answer_date']); ?></span> |
+    <div class="gray-600">
+      <div class="flex justify-between gap">
+        <div class="flex gap">
+          <a class="gray-600 flex gap-min" href="<?= url('profile', ['login' => $comment['login']]); ?>">
+            <?= Img::avatar($comment['avatar'], $comment['login'], 'img-sm', 'small'); ?>
+            <?= $comment['login']; ?>
+          </a>
+          <span class="lowercase"><?= Html::langDate($comment['answer_date']); ?></span>
+        </div>
+        <div><?= Html::votes($comment, 'answer'); ?></div>
       </div>
-      <a class="gray-600 cut-off" href="<?= url('post', ['id' => $comment['post_id'], 'slug' => $comment['post_slug']]); ?>#answer_<?= $comment['answer_id']; ?>">
+      <a class="block cut-off" href="<?= url('post', ['id' => $comment['post_id'], 'slug' => $comment['post_slug']]); ?>#answer_<?= $comment['answer_id']; ?>">
         <?= $comment['post_title']; ?>
       </a>
+      <div class="cut-post black"><?= markdown($comment['answer_content']); ?></div>
     </div>
-
-    <div class="right"><?= Html::votes($comment, 'answer'); ?></div>
-    <div class="cut-post"><?= markdown($comment['answer_content']); ?></div>
-
   <?php else : ?>
 
-    <div class="flex gap-min text-sm mb5 gray-600">
-      <div class="flex gap-min">
-        <a class="gray flex gap-min" href="<?= url('profile', ['login' => $comment['login']]); ?>">
-          <?= Img::avatar($comment['avatar'], $comment['login'], 'img-sm', 'small'); ?>
-          <?= $comment['login']; ?>
-        </a>
-        <span class="lowercase"><?= Html::langDate($comment['comment_date']); ?></span> |
+    <div class="gray">
+      <div class="flex justify-between gap">
+        <div class="flex gap">
+          <a class="gray-600 flex gap-min" href="<?= url('profile', ['login' => $comment['login']]); ?>">
+            <?= Img::avatar($comment['avatar'], $comment['login'], 'img-sm', 'small'); ?>
+            <?= $comment['login']; ?>
+          </a>
+          <span class="lowercase"><?= Html::langDate($comment['comment_date']); ?></span>
+        </div>
+
+        <div><?= Html::votes($comment, 'comment'); ?></div>
       </div>
-      <a class="gray-600 cut-off" href="<?= url('post', ['id' => $comment['post_id'], 'slug' => $comment['post_slug']]); ?>#comment_<?= $comment['comment_id']; ?>">
+      <a class="block cut-off" href="<?= url('post', ['id' => $comment['post_id'], 'slug' => $comment['post_slug']]); ?>#comment_<?= $comment['comment_id']; ?>">
         <?= $comment['post_title']; ?>
       </a>
+      <div class="cut-post black"><?= markdown($comment['comment_content']); ?></div>
     </div>
-
-    <div class="right"><?= Html::votes($comment, 'comment'); ?></div>
-    <?= markdown($comment['comment_content']); ?>
-
   <?php endif; ?>
 
 <?php endforeach; ?>
