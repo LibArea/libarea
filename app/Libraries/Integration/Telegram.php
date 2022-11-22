df61cee3e68a437e4a4e938d549eaeb7fbb36e09
Evg <budo@narod.ru>
2022-11-22 19:01:20 +0300

    DEV: Незначительные изменения

--- app/Libraries/Integration/Telegram.php
+++ app/Libraries/Integration/Telegram.php
@@ -2,8 +2,8 @@
 
 class Telegram
 {
-    
-    // https://api.telegram.org/bot<YourBOTToken>/sendMessage?chat_id=<YourCHATID>&text=<YourMESSAGE>
+    // PHP Telegram Bot based on the official Telegram Bot API
+    // We are watching: https://github.com/php-telegram-bot/core
     // Stub for Telegram
     public static function addWebhook($text, $title, $url)
     {
@@ -12,31 +12,5 @@ class Telegram
         $chat_id = config('integration.chat_id');
         
         file_get_contents($url . '/bot'. $token .'/sendMessage?chat_id='. $chat_id .'&disable_notification=true&parse_mode=HTML&text=' . $text); 
-                
-        
-        
-      /*  $text = $title . '\n' . $text;
-        
-        
-        $url = 'https://api.telegram.org';
-        $webhookurl = $url . '/bot' . config('integration.telegram_token') . '/sendMessage?chat_id=' . config('integration.chat_id') . '&text=' . $text;
-
-        $params  = [
-            'parse_mode'    => 'HTML',
-        ];  
-
-      //  Curl::index($webhookurl, $params, ['Content-type: application/json']); 
-      
-      $text = $title . '\n' . $text;
-      
-      $url = 'https://api.telegram.org';
-      $webhookurl = $url . '/bot' . config('integration.telegram_token') . '/sendMessage?chat_id=' . config('integration.chat_id') . '&text=' . $text;
-     
-     $params  = [
-            'parse_mode'    => 'HTML',
-        ];
-      
-     Curl::index($webhookurl, $params, ['Content-type: application/json']); */
-      
     }
 }
