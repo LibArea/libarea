dcc23591939aeef7825ae988ec31f2789afa0c5a
Evg <budo@narod.ru>
2023-02-27 13:03:52 +0300

    ADD: Конфигурация публикаций

---
+++ config/publication.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Publication of posts and answers
+ * Публикация постов и ответов
+ */
+
+return [
+    // Enable (true) merging the first reply from posts if post author = reply author
+    // Включить (true) слияние первого ответа с постом в том случае, если автор поста = автору ответа
+    'merge_answer_post' => false,
+    
+    // Enable (true) merge responses if a member posts multiple responses one after the other
+    // Включить (true) слияние ответов, если участник публикует множество ответов один за другим
+    'merge_answers' => false,
+]; 
