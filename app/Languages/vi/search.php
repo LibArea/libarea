ae98ade3e487ab5dfd597fd7ba08bffea33ac84c
Dzung Do <dungdo@osd.vn>
2023-09-09 23:48:16 +0700

    Create search.php

---
+++ app/Languages/vi/search.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+* The following language strings are used on the Search page.
+*
+* Следующие языковые строки используются на странице Поиска.
+*/
+
+return [
+    'all'               => 'All',
+    'posts'             => 'Posts',
+    'answers'           => 'Answers',
+    'websites'          => 'Sites',
+    'registration'      => 'Registration',
+    'sign_in'           => 'To come in',
+    'find'              => 'To find...',
+    'results_search'    => 'Results: approximately',
+    'on_website'        => 'To the website',
+    'name'              => 'Search',
+    'no_results'        => 'The search returned no results. <br>Too short / long search query',
+    'empty_request'     => 'Empty search term',
+    'search_home'       => '<h2>Search</h2>Enter a search phrase',
+    'title'             => 'Search system',
+    'desc'              => 'Search on the site and catalog taking into account morphology, facets. Search system {name}',
+    'help'              => 'Site search and more...',
+    'query'             => 'Query',
+];
