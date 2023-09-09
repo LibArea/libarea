5a1749cdcebdffda58bcc46a30c4e85398e07496
Dzung Do <dungdo@osd.vn>
2023-09-10 00:00:22 +0700

    Create meta.php

---
+++ app/Languages/vi/meta.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+* The following language strings are used for meta descriptions and explanations for pages.
+*
+* Следующие языковые строки используются для мета- описаний и пояснений к страницам.
+*/
+
+return [
+
+     // Member profile
+     // Профиль участника
+    'profile_title'       => '{name} — hồ sơ thành viên ',
+    'profile_desc'        => '{name} — trang hồ sơ thành viên. Bài đăng, giải thưởng và liên hệ. {information}',
+    
+    'profile_posts_title'   => '{name} — danh sách bài đăng ',
+    'profile_posts_all_desc'    => '{name} — danh sách bài đăng, bài viết của người tham gia được xếp theo thời gian. Toàn bộ xuất bản của site này.',
+    
+    'profile_comments_title'    => '{name} — bình luận của người tham gia ',
+    'profile_comments_desc'     => '{name} — bình luận của người tham gia được xếp theo thời gian. {information}',
+    // end
+
+    'all_users'                 => 'Danh sách người tham gia',
+    'all_users_info'            => 'All participants registered on the site',
+    'all_users_desc'            => 'Members arranged in order of their activity on the site. Ability to view profile information',
+    
+    'new_users'                 => 'Thành viên mới',
+    'new_users_info'            => 'List of members who have signed up to the site recently',
+    'new_users_desc'            => 'Members who have registered on the site recently. Show activity, avatars, profile',
+    
+    'all_blogs'                 => 'Tất cả blog',
+    'all_blogs_info'            => 'Blogs created by members',
+    'all_blogs_desc'            => 'Blogs in the order of their registration on the site, the ability to view created blogs, subscribe to them, view statistics',
+
+    'new_blogs'                 => 'New blogs',
+    'new_blogs_info'            => 'Blogs on the site added recently',
+    'new_blogs_desc'            => 'Blogs on the site added recently. Personal blogs of participants, the possibility of subscribing to interesting blogs', 
+    
+    'my_blogs'                  => 'My blogs',
+    'my_blogs_info'             => 'Blogs I follow or create',
+    'my_blogs_desc'             => 'Blogs I read or created',
+    
+    'all_topics'                => 'All topics',
+    'all_topics_info'           => 'All topics on the site in order of their popularity',
+    'all_topics_desc'           => 'A page of topics arranged in order of popularity. By the number of posted posts, the ability to subscribe to blogs',
+    
+    'new_topics'                => 'New themes',    
+    'new_topics_info'           => 'New topics, topics added recently',
+    'new_topics_desc'           => 'New themes added to the site recently. Topic statistics and the ability to subscribe to topics',
+    
+    'my_topics'                 => 'I read',
+    'my_topics_info'            => 'Topics I subscribe to',
+    'my_topics_desc'            => 'Topics I read or created',
+
+    'comments_all_info'         => 'All comments...',
+    'comments_deleted_info'     => 'Removed Comments...',
+    'answers_all_info'          => 'All answers...',
+    'answers_deleted_info'      => 'Deleted responses...',
+    
+    'comments_desc'     => 'All comments (per replies) in order of priority in the community',
+    'domain_desc'       => 'Grouping posts in the community by domain',
+    'all_comments'      => 'Tất cả lời bình',
+    
+    'feed_facet_desc'           => 'General feed. ',
+    'feed_facet_posts_desc'     => 'Post feed. ',
+    'feed_facet_questions_desc' => 'Questions thread. ',
+    'feed_facet_rec_posts_desc' => 'Recommended posts in "{name}" selected manually. ',
+    'facet_info_desc'           => 'Related information: "{name}". Wiki information, topic description. ',
+    'facet_writers_desc'        => 'The best writers in the topic: «{name}». ',
+    'facet_topics_desc'         => 'Grouping posts by topic «{name}». ',
+];
