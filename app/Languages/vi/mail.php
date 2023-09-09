4c7735b5e1e045cc3d1a0f61b9e73ad1b66dcd52
Dzung Do <dungdo@osd.vn>
2023-09-09 23:54:00 +0700

    Create mail.php

---
+++ app/Languages/vi/mail.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+* The following language strings are used to send email messages.
+*
+* Следующие языковые строки используются для отправки email сообщений.
+*/
+
+return [
+  'appealed_subject' => '{name} liên hệ với bạn (@)',
+  'appealed_message' => '<p>On the site you were contacted (@). Look:<p><p>{url}</p>',
+
+  'changing_password_subject' => '{name} — khôi phục mật khẩu',
+  'changing_password_message'  => '<p>Liên kết đặt lại mật khẩu của bạn:</p><p>{url}</p>',
+  
+  'test_subject' => '{name} — kiểm thử email (bảng quản trị)',
+  'test_message' => '<p>Email này là email kiểm thử được gửi đi từ bảng quản trị.</p>',
+   
+  'activate_email_subject' => '{name} — email xác nhận',
+  'activate_email_message' => '<p>Để tiếp tục đăng ký, kích hoạt email của bạn. Follow this link:</p><p>{url}</p>',
+   
+  'new_email_subject' => '{name} — đổi email',
+  'new_email_message' => '<p>Để tiếp tục, activate your Email by clicking on the link:</p><p>{url}</p>',
+   
+  'invite_reg_subject' => '{name} — mời để tham gia (mời)',
+  'invite_reg_message' => '<p>You have been invited to join the community. If you agree. then follow the link:</p><p>{url}</p>',
+   
+  'footer' => '<p>This email was generated automatically.<br>You don\'t need to reply to it.</p><p>Administration.<br>{name}</p>',
+];
