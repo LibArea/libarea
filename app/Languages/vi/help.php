58d7bf1832ab52540356dc14c66de2a86e526fce
Dzung Do <dungdo@osd.vn>
2023-09-10 00:07:54 +0700

    Create help.php

---
+++ app/Languages/vi/help.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+* The following language strings are used for translating blocks 
+* mostly located in the sidebar.
+*
+* Следующие языковые строки используются для переводя блоков 
+* преимущественно расположенных в боковой панели (sidebar).
+*/
+
+return [
+    'setting_info'          => 'Fill in your name, this will help the participants to refer to you by your first name. <br><br> Tell us something about yourself. You have a great opportunity to open up to please others and seem like an interesting person.',
+    'avatar_info'           => 'Upload an avatar and cover photo for your profile. <br><br> When each user has a unique avatar, it becomes much easier to follow discussions!',
+    'security_info'         => 'Make sure all your passwords are 8 characters or more, ideally a lot more.',
+    'recover_info'          => 'After you click the "Reset" button, check your e-mail.',
+    'favorite_info'         => 'Posts, comments and questions in your bookmarks.',
+    'subscribed_info'       => 'Posts you follow. You will be notified of all new replies.',
+    'read_info'             => 'Read posts. History of the last 50 views.',
+    'notifications_info'    => 'You can mark all ads as read by clicking on the link: "I have read".',
+    'notification_info'     => 'You can unsubscribe from all email notifications in this section.',
+
+    'add_post'              => 'Use the tag <b>cut</b> (<svg class="icons"><use xlink:href="/assets/svg/icons.svg#cut"></use></svg>).<br><br>  It hides under the cat part of the text following the tag (it will be written "Read more").',
+    'edit_page'             => 'Official documentation should be error-free and written more in a formal manner.',
+    'edit_post'             => 'See what the post looks like on the feed page and in the full version. Improve it here.',
+    'add_topic'             => 'A topic is a node (for navigation) used by the system to organize content. The theme is a single point of view, the interpretation of various things, phenomena and other concepts.',
+    'add_blog'              => 'Categories refer to the directory (sites, programs). Create them only when there are enough sites to fill.',
+    'add_category'          => 'Categories are used in the site directory. Do not create empty categories.',
+    'edit_category'         => 'Edit the category to reflect the sites in it. Write a correct description and choose a short title.',
+    'add_section'           => 'Sections are used for grouping (navigation) by service articles.',
+    'edit_topic'            => 'Topics are used to group (navigate) posts. This navigation, take it seriously.',
+    'edit_blog'             => 'Blogs are usually created by contributors. The blog is assigned to a specific user.',
+    'edit_section'          => 'This is navigation editing for service pages (documentation). Take it seriously.',
+
+    'team_info'             => 'Other participants can also post articles to your Blog — for this you need to grant them access.',
+    'draft_info'            => 'Only the author can read drafts.',
+    
+    'add_poll'              => 'Fill in all the fields. This survey can be used in other parts of the site (for example, in posts).',
+    'poll_info'             => 'Bầu chọn của tôi theo thời gian.',
+];
