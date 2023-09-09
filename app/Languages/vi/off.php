76049ea219167629598366cd85f32d4aa989ef81
Dzung Do <dungdo@osd.vn>
2023-09-09 23:47:39 +0700

    Create off.php

---
+++ app/Languages/vi/off.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+|
+| The following language strings are used on the site's login 
+| page when the site is disabled and has staff-only access.
+|
+| Следующие языковые строки используются на странице входа на сайт, 
+| когда он отключен и имеет доступ только для персонала.
+|
+*/
+
+return [
+    'opss'                  => 'Ố ồ',
+    'enter'                 => 'Truy cập',
+    'email'                 => 'Email',
+    'password'              => 'Mật khẩu',
+    'sign_in'               => 'Đăng nhập',
+    'enter_password'        => 'Điền mật khẩu',
+    'under_reconstruction'  => 'Site đang được tái cấu trúc. Lối vào chỉ dành cho người quản lý.',
+];
