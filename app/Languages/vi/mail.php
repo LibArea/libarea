37b9699e978b8226ac1599af5f93b15d2c6bf2de
Dzung Do <dungdo@osd.vn>
2023-09-12 00:43:13 +0700

    Update mail.php

--- app/Languages/vi/mail.php
+++ app/Languages/vi/mail.php
@@ -8,7 +8,7 @@
 
 return [
   'appealed_subject' => '{name} liên hệ với bạn (@)',
-  'appealed_message' => '<p>On the site you were contacted (@). Look:<p><p>{url}</p>',
+  'appealed_message' => '<p>Từ site bạn được liên hệ (@). Hãy nhìn vào:<p><p>{url}</p>',
 
   'changing_password_subject' => '{name} — khôi phục mật khẩu',
   'changing_password_message'  => '<p>Liên kết đặt lại mật khẩu của bạn:</p><p>{url}</p>',
@@ -17,13 +17,13 @@ return [
   'test_message' => '<p>Email này là email kiểm thử được gửi đi từ bảng quản trị.</p>',
    
   'activate_email_subject' => '{name} — email xác nhận',
-  'activate_email_message' => '<p>Để tiếp tục đăng ký, kích hoạt email của bạn. Follow this link:</p><p>{url}</p>',
+  'activate_email_message' => '<p>Để tiếp tục đăng ký, kích hoạt email của bạn. Hãy theo liên kết này:</p><p>{url}</p>',
    
   'new_email_subject' => '{name} — đổi email',
-  'new_email_message' => '<p>Để tiếp tục, activate your Email by clicking on the link:</p><p>{url}</p>',
+  'new_email_message' => '<p>Để tiếp tục, để kích hoạt email của bạn, nhấp chuột vào liên kết:</p><p>{url}</p>',
    
   'invite_reg_subject' => '{name} — mời để tham gia (mời)',
-  'invite_reg_message' => '<p>You have been invited to join the community. If you agree. then follow the link:</p><p>{url}</p>',
+  'invite_reg_message' => '<p>Bạn đã được mời tham gia cộng đồng trực tuyến. Để chấp thuận, vui lòng theo liên kết sau:</p><p>{url}</p>',
    
-  'footer' => '<p>This email was generated automatically.<br>You don\'t need to reply to it.</p><p>Administration.<br>{name}</p>',
+  'footer' => '<p>Email được sinh tự động.<br>Bạn không cần trả lời email này.</p><p>Quản trị viên.<br>{name}</p>',
 ];
