20ff9885f6c5ee1c3a94ad3b3b5b09221084fc73
Evg <budo@narod.ru>
2023-10-20 04:14:18 +0300

    DEV: Локализация

--- resources/views/default/_block/form/add-comment.php
+++ resources/views/default/_block/form/add-comment.php
@@ -6,14 +6,14 @@
       <fieldset>
         <input type="hidden" name="answer_id" id="answer_id" value="<?= $data['answer_id']; ?>">
         <input type="hidden" name="comment_id" id="comment_id" value="<?= $data['comment_id']; ?>">
-        <?= Html::sumbit(__('app.comment')); ?>
+        <?= Html::sumbit(__('app.reply')); ?>
         <span id="cancel_comment" class="text-sm inline ml5 gray"><?= __('app.cancel'); ?></span>
       </fieldset>
     </form>
   <?php else : ?>
     <textarea rows="5" disabled="disabled" placeholder="<?= __('app.auth_comm'); ?>."></textarea>
     <div>
-      <?= Html::sumbit(__('app.comment')); ?>
+      <?= Html::sumbit(__('app.reply')); ?>
       <span id="cancel_comment" class="text-sm inline ml5 gray"><?= __('app.cancel'); ?></span>
     </div>
   <?php endif; ?>
