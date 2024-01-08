c460b41bc4828714489ba4abed75f66ab9c6fe47
Evg <budo@narod.ru>
2024-01-08 05:36:09 +0300

    DEV: Предпочтения
    См. https://libarea.ru/post/1652/add-predpochteniya-fasety

--- resources/views/default/content/user/setting/preferences.php
+++ resources/views/default/content/user/setting/preferences.php
@@ -15,23 +15,28 @@
           <?= __('app.signed_facets_help', ['num' => config('facets.quantity_home')]); ?>
         </span>
 
-        <div class="tasks__list mt15 mb15">
-          <?php foreach ($data['facets'] as $facet) : ?>
-            <div class="flex flex-row items-center gap-min mb15 tasks__item">
-              <input name="id[]" value="<?= $facet['facet_id']; ?>" type="checkbox" <?php if ($facet['facet_output']) : ?> checked<?php endif; ?>>
-              <a title="<?= $facet['facet_title']; ?>" href="<?= url($facet['facet_type'], ['slug' => $facet['facet_slug']]); ?>">
-                <?= Img::image($facet['facet_img'], $facet['facet_title'], 'img-sm', 'logo', 'max'); ?>
-              </a>
-              <?= $facet['facet_title']; ?>
-              <?php if ($facet['facet_type'] == 'blog') : ?><sup class="red lowercase"><?= __('app.blog'); ?></sup><?php endif; ?>
-            </div>
-          <?php endforeach; ?>
-        </div>
-        <div class="mb15">
-          <?= Html::pagination($data['pNum'], $data['pagesCount'], null, 'preferences', '?'); ?>
-        </div>
-
-        <?= Html::sumbit(__('app.edit')); ?>
+        <?php if ($data['facets']) : ?>
+          <div class="tasks__list mt15 mb15">
+            <?php foreach ($data['facets'] as $facet) : ?>
+              <div class="flex flex-row items-center gap-min mb15 tasks__item">
+                <input name="id[]" value="<?= $facet['facet_id']; ?>" type="checkbox" <?php if ($facet['facet_output']) : ?> checked<?php endif; ?>>
+                <a title="<?= $facet['facet_title']; ?>" href="<?= url($facet['facet_type'], ['slug' => $facet['facet_slug']]); ?>">
+                  <?= Img::image($facet['facet_img'], $facet['facet_title'], 'img-sm', 'logo', 'max'); ?>
+                </a>
+                <?= $facet['facet_title']; ?>
+                <?php if ($facet['facet_type'] == 'blog') : ?><sup class="red lowercase"><?= __('app.blog'); ?></sup><?php endif; ?>
+              </div>
+            <?php endforeach; ?>
+          </div>
+          <div class="mb15">
+            <?= Html::pagination($data['pNum'], $data['pagesCount'], null, 'preferences', '?'); ?>
+          </div>
+          <?= Html::sumbit(__('app.edit')); ?>
+        <?php else : ?>
+          <div class="mt15 mb15">
+            <a class="btn btn-outline-primary center" href="/topics"><?= __('app.topic_subs'); ?></a>
+          </div>
+        <?php endif; ?>
       </form>
     </div>
 </main>
