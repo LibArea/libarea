348ba9f584d7d24c20e59070e2b62e1b0a1b4a3c
Evg <budo@narod.ru>
2024-01-25 05:08:15 +0300

    DEV: Рефакторинг (3)
    Продолжение см.: https://github.com/LibArea/libarea/commit/10dba83cbb4d79d2b2de056ed95d9a3d731ef616

--- resources/views/default/js/catalog.js
+++ resources/views/default/js/catalog.js
@@ -1,16 +1,10 @@
-const favicon = queryAll(".add-favicon"),
-  sturl = queryAll(".status"),
-  screenshot = queryAll(".add-screenshot");
+const favicon = queryAll(".add-favicon");
+const statusurl = queryAll(".status");
+const screenshot = queryAll(".add-screenshot");
 
-// Write down a Favicon
-favicon.forEach(el => el.addEventListener("click", function (e) {
-  makeRequest("/web/favicon/add", options = { body: "id=" + el.dataset.id })
-}));
-
-// Write down a Screenshot
-screenshot.forEach(el => el.addEventListener("click", function (e) {
-  makeRequest("/web/screenshot/add", options = { body: "id=" + el.dataset.id })
-}));
+favicon.forEach(el => el.addEventListener("click", () => makeRequest("/web/favicon/add", { body: `id=${el.dataset.id}` })));
+screenshot.forEach(el => el.addEventListener("click", () => makeRequest("/web/screenshot/add", { body: `id=${el.dataset.id}` })));
+statusurl.forEach(el => el.addEventListener("click", () => makeRequest("/web/status/update")));
 
 // search
 isIdEmpty('find-url').onclick = function () {
@@ -19,51 +13,42 @@ isIdEmpty('find-url').onclick = function () {
   });
 }
 
-// URL Status Update
-sturl.forEach(el => el.addEventListener("click", function (e) {
-  makeRequest("/web/status/update");
-}));
-
 function fetchSearchUrl() {
   let url = getById("find-url").value;
   if (url.length < 5) return;
 
+  const body = `url=${url}&_token=${token}`;
+
   fetch("/search/web/url", {
-    method: "POST",
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    body: "url=" + url + "&_token=" + token,
-  })
-    .then(
-      response => {
-        return response.text();
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: body,
+    })
+    .then(response => response.text())
+    .then(text => {
+      const obj = JSON.parse(text);
+      let html = '<div class="flex">';
+      for (const key in obj) {
+        if (obj[key].item_id) {
+          html += `<a class="block green text-sm mt5 mb5" href="/web/website/${obj[key].item_id}">${obj[key].item_url}</a>`;
+        }
       }
-    ).then(
-      text => {
-        let obj = JSON.parse(text);
-        let html = '<div class="flex">';
-        for (let key in obj) {
-          if (obj[key].item_id) {
-            html += '<a class="block green text-sm mt5 mb5" href="/web/website/' + obj[key].item_id + '">' + obj[key].item_url + '</a>';
+      html += "</div>";
+
+      if (Object.keys(obj).length !== 0) {
+        const items = getById("search_url");
+        items.classList.add("block");
+        items.innerHTML = html;
+      }
+
+      const menu = document.querySelector(".none.block");
+      if (menu) {
+        document.onclick = function (event) {
+          if (!event.target.classList.contains("none.block")) {
+            const items = getById("search_url");
+            items.classList.remove("block");
           }
-
-          html += '</div>';
-        }
-
-        if (!Object.keys(obj).length == 0) {
-          let items = getById("search_url");
-          items.classList.add("block");
-          items.innerHTML = html;
-        }
-
-        let menu = document.querySelector('.none.block');
-        if (menu) {
-          document.onclick = function (e) {
-            if (event.target.className != '.none.block') {
-              let items = getById("search_url");
-              items.classList.remove("block");
-            };
-          };
-        }
+        };
       }
-    );
+    });
 }
