8ab26dcbc32209df803347b3e06018e2c07266d2
Evg <budo@narod.ru>
2024-07-20 10:04:31 +0300

    FIX: ярлык поста

--- resources/views/default/content/post/post-title.php
+++ resources/views/default/content/post/post-title.php
@@ -5,7 +5,7 @@
 <?php if ($post['post_feature'] == 1) : ?> <span class="label label-green"><?= __('app.question'); ?></span> <?php endif; ?>
 <?php if ($post['post_translation'] == 1) : ?><span class="label label-grey"><?= __('app.translation'); ?></span><?php endif; ?>
 <?php if ($post['post_tl']) : ?> <span class="brown italic text-sm ml5">tl<?= $post['post_tl']; ?></span> <?php endif; ?>
-<?php if ($post['post_nsfw']) : ?> <span class="red text-sm ml5">NFSW</span> <?php endif; ?>
+<?php if ($post['post_nsfw']) : ?> <span class="red text-sm ml5">NSFW</span> <?php endif; ?>
 <?php if ($post['post_hidden']) : ?> <svg class="icons gray-600"><use xlink:href="/assets/svg/icons.svg#eye"></use></svg> <?php endif; ?>
 <?php if ($post['post_merged_id']) : ?>
   <span class="red">
