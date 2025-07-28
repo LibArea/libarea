83fe5cb90f1c11b9728999e3eba6e2fb7a59f186
Evg <budo@narod.ru>
2025-07-28 17:08:44 +0300

    ADD:  SVG

--- resources/views/default/content/publications/title.php
+++ resources/views/default/content/publications/title.php
@@ -1,6 +1,6 @@
 <?php if ($item['post_is_deleted'] == 1) : ?><span class="label label-orange"><?= __('app.remote'); ?></span><?php endif; ?>
 <?php if ($item['post_closed'] == 1) : ?><span class="gray-600" title="<?= __('app.close'); ?>"><svg class="icon"><use xlink:href="/assets/svg/icons.svg#lock"></use></svg></span><?php endif; ?>
-<?php if ($item['post_top'] == 1) : ?> <sup class="red"><svg class="icon"><use xlink:href="/assets/svg/icons.svg#arrow-up"></use></svg></sup> <?php endif; ?>
+<?php if ($item['post_top'] == 1) : ?> <sup class="red"><svg class="icon"><use xlink:href="/assets/svg/icons.svg#pin"></use></svg></sup> <?php endif; ?>
 <?php if ($item['post_lo']) : ?><span class="red"><svg class="icon"><use xlink:href="/assets/svg/icons.svg#selected"></use></svg></span><?php endif; ?>
 <?php if ($item['post_translation'] == 1) : ?><span class="label label-grey"><?= __('app.translation'); ?></span><?php endif; ?>
 <?php if ($item['post_tl']) : ?> <span class="brown italic text-sm ml5">tl<?= $item['post_tl']; ?></span> <?php endif; ?>
