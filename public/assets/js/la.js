98f3e7d54c2156cc7867969836f53690edec5d03
Evg <budo@narod.ru>
2025-07-29 10:21:30 +0300

    DEV: Обновим собранные файлы css, js

--- public/assets/js/la.js
+++ public/assets/js/la.js
@@ -5,4 +5,3 @@ function makeRequest(url,options={}){return fetch(url,{...options,method:"POST",
 function getDefaultTime(){let expirationDate=new Date();expirationDate.setTime(expirationDate.getTime()+(365*24*60*60*1000));return"expires="+expirationDate.toGMTString()}
 function getCookie(cookieName){let name=cookieName+"=";let cookieArray=document.cookie.split(';');for(let i=0;i<cookieArray.length;i++){let cookie=cookieArray[i].trim();if(cookie.indexOf(name)===0){return cookie.substring(name.length)}}
 return""}
-function toggleMode(cookieName,firstMode,secondMode){let mode=getCookie(cookieName);let expires=getDefaultTime();if(mode==firstMode){document.cookie=cookieName+"="+secondMode+"; "+expires+";path=/";document.getElementsByTagName('body')[0].classList.remove(firstMode)}else{document.cookie=cookieName+"="+firstMode+"; "+expires+";path=/";document.getElementsByTagName('body')[0].classList.add(firstMode)}}
