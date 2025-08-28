624142c4fce8ee0d981ac960b14a4b18b6472a12
Evg <budo@narod.ru>
2025-08-29 01:37:22 +0300

    Update facet-type.php

--- resources/views/default/_block/form/facet-type.php
+++ resources/views/default/_block/form/facet-type.php
@@ -2,11 +2,14 @@
   <label for="post_content"><?= __('app.type'); ?></label>
   <select name="facet_type">
     <?php foreach (App\Controllers\Facet\FacetController::types() as $value) : ?>
-      <?php if ($container->access()->limitTl(config('trust-levels', 'tl_add_' . $value['type_code']))) : ?>
-        <option <?php if (!empty($type)) : ?><?php if ($value['type_code'] == $type) : ?>selected<?php endif; ?><?php endif; ?> value="<?= $value['type_code']; ?>">
-          <?= __('app.' . $value['type_lang']); ?>
-        </option>
-      <?php endif; ?>
+	  <?php // из базы "Категория" не стоит удалять, запретим тут для UX
+	  if ($value['type_code'] != 'category') : ?>
+        <?php if ($container->access()->limitTl(config('trust-levels', 'tl_add_' . $value['type_code']))) : ?>
+          <option <?php if (!empty($type)) : ?><?php if ($value['type_code'] == $type) : ?>selected<?php endif; ?><?php endif; ?> value="<?= $value['type_code']; ?>">
+            <?= __('app.' . $value['type_lang']); ?>
+          </option>
+        <?php endif; ?>
+	  <?php endif; ?>
     <?php endforeach; ?>
   </select>
 </fieldset>
