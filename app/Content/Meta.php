e99e52afe2f6639dd4f63bfca87623e7ae863069
Evg <budo@narod.ru>
2026-02-03 13:20:46 +0300

    Update Meta.php

--- app/Content/Meta.php
+++ app/Content/Meta.php
@@ -182,41 +182,44 @@ class Meta
 
     public static function facet(string $sheet, array $facet, string $type = 'topic', array $topic = []): string
     {
+		$seo_title = htmlEncode($facet['facet_seo_title']);
+		$seo_description = htmlEncode($facet['facet_description']);
+		
         switch ($sheet) {
             case 'questions':
                 $url    = url($type . '.questions', ['slug' => $facet['facet_slug']]);
-                $title  = $facet['facet_seo_title'] . ' — ' . __('app.questions');
-                $description = __('meta.feed_facet_questions_desc') . $facet['facet_description'];
+                $title  = $seo_title . ' — ' . __('app.questions');
+                $description = __('meta.feed_facet_questions_desc') . $seo_description;
                 break;
             case 'posts':
                 $url    = url($type . '.posts', ['slug' => $facet['facet_slug']]);
-                $title  = $facet['facet_seo_title'] . ' — ' . __('app.posts');
-                $description = __('meta.feed_facet_posts_desc') . $facet['facet_description'];
+                $title  = $seo_title . ' — ' . __('app.posts');
+                $description = __('meta.feed_facet_posts_desc') . $seo_description;
                 break;
             case 'recommend':
                 $url    =  url($type . '.recommend', ['slug' => $facet['facet_slug']]);
-                $title  = $facet['facet_seo_title'] . ' — ' .  __('app.rec_posts');
-                $description  = __('meta.feed_facet_rec_posts_desc', ['name' => $facet['facet_seo_title']]) . $facet['facet_description'];
+                $title  = $seo_title . ' — ' .  __('app.rec_posts');
+                $description  = __('meta.feed_facet_rec_posts_desc', ['name' => $seo_title]) . $seo_description;
                 break;
             case 'info':
                 $url    = url($type . '.info', ['slug' => $facet['facet_slug']]);
-                $title  = $facet['facet_seo_title'] . ' — ' . __('app.info');
-                $description = __('meta.facet_info_desc', ['name' => $facet['facet_seo_title']]) . $facet['facet_description'];
+                $title  = $seo_title . ' — ' . __('app.info');
+                $description = __('meta.facet_info_desc', ['name' => $seo_title]) . $seo_description;
                 break;
             case 'writers':
                 $url    = url($type . '.writers', ['slug' => $facet['facet_slug']]);
-                $title  = $facet['facet_seo_title'] . ' — ' . __('app.writers');
-                $description = __('meta.facet_writers_desc', ['name' => $facet['facet_seo_title']]) . $facet['facet_description'];
+                $title  = $seo_title . ' — ' . __('app.writers');
+                $description = __('meta.facet_writers_desc', ['name' => $seo_title]) . $seo_description;
                 break;
             case 'blog.topics':
                 $url    = url($type . '.topics', ['slug' => $facet['facet_slug'], 'tslug' => $topic['facet_slug']]);
-                $title  = $topic['facet_seo_title'] . ' — ' . $facet['facet_seo_title'];
-                $description = __('meta.facet_topics_desc', ['name' => $topic['facet_seo_title']]) . $facet['facet_description'];
+                $title  = $seo_title . ' — ' . __('app.topics');
+                $description = __('meta.facet_topics_desc', ['name' => $seo_title]) . $seo_description;
                 break;
             default: // facet.feed 
                 $url    = url($type, ['slug' => $facet['facet_slug']]);
-                $title  = $facet['facet_seo_title'] . ' — ' .  __('app.feed');
-                $description = __('meta.feed_facet_desc') . $facet['facet_description'];
+                $title  = $seo_title . ' — ' .  __('app.feed');
+                $description = __('meta.feed_facet_desc') . $seo_description;
         }
 
         $meta = [
