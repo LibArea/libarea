cd71ef9bcdbced16f6258a658e843b9553063f63
Evg <budo@narod.ru>
2026-04-24 05:32:56 +0300

    Update no-content.php

--- resources/views/default/_block/no-content.php
+++ resources/views/default/_block/no-content.php
@@ -1,11 +1,11 @@
 <?php if ($type == 'small') : ?>
   <div class="box">
-    <?= icon('icons', $icon, '128', 'max green'); ?>
-    <span class="middle"><?= $text; ?></span>
+    <?= icon('icons', $icon, 25, 'icon green'); ?>
+    <span class="middle"><?= $text; ?>.</span>
   </div>
 <?php else : ?>
   <div id="no_content" class="p15 center gray-600">
-    <?= icon('icons', $icon, '128'); ?>
+    <?= icon('icons', $icon, 128); ?>
     <div><?= $text; ?></div>
   </div>
 <?php endif; ?>
